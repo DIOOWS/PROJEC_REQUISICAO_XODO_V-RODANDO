a37d4acc601b55f36b27bdc19e7649f64b67f127
Diogo Silva <dioowdioowtrd@gmail.com>
2025-12-10 01:53:54 -0300

    correção

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,15 +1,22 @@
-from .models import Order
-
 def global_settings(request):
+    from .models_config import SystemConfig
+
+    try:
+        config = SystemConfig.objects.first()
+        system_logo = config.logo_url if config else ""
+    except Exception:
+        system_logo = ""
+
     try:
         if request.user.is_authenticated and request.user.is_staff:
+            from .models import Order
             pending_orders = Order.objects.filter(is_read=False).count()
         else:
             pending_orders = 0
-    except Exception:
-        # Se der erro no banco, não quebra a página
+    except:
         pending_orders = 0
 
     return {
-        "pending_orders": pending_orders
+        "pending_orders": pending_orders,
+        "SYSTEM_LOGO": system_logo,
     }
